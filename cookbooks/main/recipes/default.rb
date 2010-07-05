2f5d4f4b040abd597051ac77907a037dcf1cbf6b
joshcutler <josh.cutler@gmail.com>
2010-07-05 18:11:10 -0500

    Removed DJ

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -16,7 +16,7 @@
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 require_recipe "sphinx"
 
-require_recipe "delayed_job"
+#require_recipe "delayed_job"
 
 #uncomment to turn on memcached
 # require_recipe "memcached"
